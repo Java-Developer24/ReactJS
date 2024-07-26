a7ad478289c65bfbee8f97de3553c5a2dc961fcf
Developer@2446 <105372372+Java-Developer24@users.noreply.github.com>
2024-07-26 08:04:47 +0000

    V2

--- Social-Media-app-Using Fetch/src/components/PostList.jsx
+++ Social-Media-app-Using Fetch/src/components/PostList.jsx
@@ -1,25 +1,46 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import {PostList as PostListData }from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList=()=>{
     const{postList,addIntialPosts}=useContext(PostListData)
+    const [fetching, setFetching]=useState(false);
+    
+    
     
 
-    // useEffect(()=>{ fetch('https://dummyjson.com/posts')
-    //     .then(res => res.json())
-    //     .then((data)=>{
-    //         addIntialPosts(data.posts);})},[]);
+    useEffect(()=>{
+        const controller=new AbortController();
+        const signal=controller.signal
+        setFetching(true)
+        fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+        .then((data)=>{
+            addIntialPosts(data.posts);
+            setFetching(false);
+        })
+    
+    return ()=>{
 
-            const handleGetPostFromserver=()=>{ 
-                fetch('https://dummyjson.com/posts')
-                .then(res => res.json())
-                .then((data)=>{
-                    addIntialPosts(data.posts)})}
+        console.log("useEffect aborted");
+        controller.abort();
+    }},
+    
+        []);
+
+            
+            // const handleGetPostFromserver=()=>{ 
+            //     fetch('https://dummyjson.com/posts')
+            //     .then(res => res.json())
+            //     .then((data)=>{
+            //         addIntialPosts(data.posts)})}
   
 return<>
-{postList.length===0&&<WelcomeMessage handlePostData= {handleGetPostFromserver} />}
-{postList.map((post)=>(
+{fetching&&<LoadingSpinner/>}
+{!fetching&&postList.length===0&&<WelcomeMessage  />}
+{/* {postList.length===0&&<WelcomeMessage handlePostData= {handleGetPostFromserver} />} */}
+{!fetching&&postList.map((post)=>(
 <Post key={post.id} post={post}/>
     ))}
 
--- Social-Media-app-Using Fetch/src/components/WelcomeMessage.jsx
+++ Social-Media-app-Using Fetch/src/components/WelcomeMessage.jsx
@@ -1,7 +1,7 @@
 const WelcomeMessage=({handlePostData})=>{
     return(<center  className="welcome-message"><h1>There is no post</h1>
  
-            <button type="button" className="btn btn-primary"  onClick={handlePostData}>Get post from server</button>
+            {/* <button type="button" className="btn btn-primary"  onClick={handlePostData}>Get post from server</button> */}
     
     
     </center>)
